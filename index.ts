8f5c9337ebe302a1bd500cd46619ab7707e57c35
Yousuf Razzak <usuf.razzak12344321@gmail.com>
2024-03-25 20:52:59 +0500

    created number guessing game finally

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber= Math.floor(Math.random() *10 +1);
+
+
+const answer = await inquirer.prompt([
+    {
+        message: "Please guess a number between 1 to 10:",
+        type: "number",
+        name: "gNumber",
+    }
+]);
+
+if (answer.gNumber === randomNumber) {
+    console.log("congratulation! You guess the right number");
+}
+else{
+    console.log("Please guess the right number");
+}
+
